40bc299b8e94db8fef3cffde0431abd834626b4b
Binshion <binshion@hotmail.com>
2018-10-18 22:29:39 +0800

    dev分支提交

--- thinkingInJava/interfaces/Factories.java
+++ thinkingInJava/interfaces/Factories.java
@@ -61,6 +61,5 @@ public class Factories {
     public static void main(String[] args){
         serviceConsumer(new Implementation1Factory());
         serviceConsumer(new Implementation2Factory());
-
     }
 }
