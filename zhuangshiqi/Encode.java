8e9cf5632cce9753896644e8fd81e70a6ca8b7bc
Binshion <binshion@hotmail.com>
2019-01-14 00:00:59 +0800

    434

--- zhuangshiqi/Encode.java
+++ zhuangshiqi/Encode.java
@@ -1,5 +1,9 @@
 package zhuangshiqi;
 
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.MimeUtility;
+
+import java.net.URLEncoder;
+
 /**
  * 转码测试
  * 原文件名： 科目余额表.xls
@@ -14,7 +18,14 @@ public class Encode {
 
         String newFilename = new String(filename.getBytes("UTF-8"), "ISO-8859-1");
         String fi = new String(newFilename.getBytes("ISO-8859-1"), "UTF-8");
-        System.out.println(newFilename);
-        System.out.println(fi);
+        String f2 = new String(newFilename.getBytes("ISO-8859-1"), "ascii");
+        System.out.println("UTF-8 转ISO :" + newFilename);
+        System.out.println("ISO逆向UTF8 :" + fi);
+        System.out.println("URLEncoder:" + URLEncoder.encode(filename, "utf-8"));
+
+        System.out.println(f2);
+
+        String f3 = new String(URLEncoder.encode(filename).getBytes("utf8"), "utf8");
+        System.out.println(f3);
     }
 }
