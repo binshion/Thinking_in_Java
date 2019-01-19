bf2102588800626fa759868d005462393125dfda
Binshion <binshion@hotmail.com>
2019-01-19 21:32:14 +0800

    15.11.3 转型和警告

---
+++ thinkingInJava/generics/NeedCasting.java
@@ -0,0 +1,12 @@
+package thinkingInJava.generics;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.List;
+
+public class NeedCasting {
+    public void f(String[] args) throws Exception {
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(args[0]));
+        List<Widget> shapes = (List<Widget>) in.readObject();
+    }
+}
