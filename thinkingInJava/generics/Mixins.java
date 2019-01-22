3919a6309f7829799d17961d512456804fa58811
binshion <binshion@hotmail.com>
2019-01-22 19:41:45 +0800

    15.15.2 与接口混合

--- thinkingInJava/generics/Mixins.java
+++ thinkingInJava/generics/Mixins.java
@@ -18,5 +18,63 @@ class TimeStampedImp implements TimeStamped {
     }
 }
 
+interface SerialNumbered {
+    long getSerialNumber();
+}
+
+class SerialNumberedImp implements SerialNumbered {
+    private static long counter = 1;
+    private final long serialNumber = counter++;
+
+    @Override
+    public long getSerialNumber() {
+        return serialNumber;
+    }
+}
+
+interface Basic {
+    public void set(String val);
+    public String get();
+}
+
+class BasicImp implements Basic {
+    private String value;
+    @Override
+    public void set(String val) {
+        value = val;
+    }
+
+    @Override
+    public String get() {
+        return value;
+    }
+}
+
+class Mixin extends BasicImp implements TimeStamped,
+        SerialNumbered {
+    private TimeStamped timeStamp = new TimeStampedImp();
+    private SerialNumbered serialNumber = new SerialNumberedImp();
+
+    @Override
+    public long getStamp() {
+        return timeStamp.getStamp();
+    }
+
+    @Override
+    public long getSerialNumber() {
+        return serialNumber.getSerialNumber();
+    }
+}
+
 public class Mixins {
+    public static void main(String[] args){
+        Mixin mixin1 = new Mixin(), mixin2 = new Mixin();
+        mixin1.set("test string 1");
+        mixin2.set("test string 2");
+        System.out.println(mixin1.get() + " " +
+            mixin1.getStamp() + " " + mixin1.getSerialNumber());
+
+        System.out.println(mixin2.get() + " " +
+        mixin2.getStamp() + " " + mixin2.getSerialNumber());
+    }
 }
