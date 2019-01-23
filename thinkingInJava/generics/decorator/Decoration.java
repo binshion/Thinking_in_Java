2bed144187cc53d7d14848b3bcf36eaaff4568d7
binshion <binshion@hotmail.com>
2019-01-23 12:07:49 +0800

    15.15.3 使用装饰器模式

--- thinkingInJava/generics/decorator/Decoration.java
+++ thinkingInJava/generics/decorator/Decoration.java
@@ -1,5 +1,7 @@
 package thinkingInJava.generics.decorator;
 
+import java.util.Date;
+
 class Basic {
     private String value;
 
@@ -27,5 +29,36 @@ class Decorator extends Basic {
     }
 }
 
+class TimeStamped extends Decorator {
+    private final long timeStamp;
+    public TimeStamped(Basic basic) {
+        super(basic);
+        timeStamp = new Date().getTime();
+    }
+
+    public long getStamp() {
+        return timeStamp;
+    }
+}
+
+class SerialNumbered extends Decorator {
+    private static long counter = 1;
+    private final long serialNumber = counter++;
+    public SerialNumbered(Basic basic) {
+        super(basic);
+    }
+
+    public long getSerialNumber() {
+        return serialNumber;
+    }
+}
+
 public class Decoration {
+    public static void main(String[] args){
+        TimeStamped t = new TimeStamped(new Basic());
+        TimeStamped t2 = new TimeStamped(new SerialNumbered(new Basic()));
+
+        SerialNumbered s = new SerialNumbered(new Basic());
+        SerialNumbered s2 = new SerialNumbered(new TimeStamped(new Basic()));
+    }
 }
