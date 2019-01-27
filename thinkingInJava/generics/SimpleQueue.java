17e21d2c2092b289760dc4be863581b471c96b00
Binshion <binshion@hotmail.com>
2019-01-27 23:49:11 +0800

    15.17.2 将一个方法应用于序列

---
+++ thinkingInJava/generics/SimpleQueue.java
@@ -0,0 +1,19 @@
+package thinkingInJava.generics;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class SimpleQueue<T> implements Iterable<T> {
+    private LinkedList<T> storage = new LinkedList<>();
+    public void add(T t) {
+        storage.offer(t);
+    }
+    public T get() {
+        return storage.poll();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return storage.iterator();
+    }
+}
