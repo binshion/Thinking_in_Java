53d3b37d09cf367890975e911ad84c3a3583eee8
binshion <binshion@hotmail.com>
2019-01-29 19:29:14 +0800

    16.5 数组与泛型

---
+++ thinkingInJava/arrays/ParameterizedArrayType.java
@@ -0,0 +1,23 @@
+package thinkingInJava.arrays;
+
+class ClassParameter<T> {
+    public T[] f(T[] arg) {
+        return arg;
+    }
+}
+
+class MethodParameter {
+    public static <T> T[] f(T[] arg) {
+        return arg;
+    }
+}
+
+public class ParameterizedArrayType {
+    public static void main(String[] args){
+        Integer[] ints = {1, 2, 3, 4, 5};
+        Double[] doubles = {1.1, 2.2, 3.3, 4.4, 5.5};
+
+        Integer[] ints2 = new ClassParameter<Integer>().f(ints);
+        Double[] doubles2 = MethodParameter.f(doubles);
+    }
+}
