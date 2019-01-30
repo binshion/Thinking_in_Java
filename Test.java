018e415a27c3f425cb7549c4b72337a211e5d812
Binshion <binshion@hotmail.com>
2019-01-31 00:01:34 +0800

    0131

--- Test.java
+++ Test.java
@@ -1,21 +1,17 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Test {
-    public Test(){
-
-    }
+    public Test(){}
     
     public Test(int a){
         System.out.println(a);
     }
+
     public static void main(String[] args){
-        Test t = new Test();
-        byte value = 0;
-        byte b = s(value);
-        System.out.println(b);
-
-    }
-
-    public static byte s(byte a) {
-        return ++a;
+        BigDecimal d1 = new BigDecimal("0.8000");
+        BigDecimal d2 = new BigDecimal("1");
+        BigDecimal d3 = d2.divide(d1, 4, RoundingMode.HALF_UP);
+        System.out.println(d3);
     }
 }
