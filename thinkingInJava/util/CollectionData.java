171e856e586c75c2148726acf10bce54a383e4ca
Binshion <binshion@hotmail.com>
2019-02-03 23:35:30 +0800

    16.6.3 从Generator中创建数组

--- thinkingInJava/util/CollectionData.java
+++ thinkingInJava/util/CollectionData.java
@@ -1,4 +1,15 @@
 package thinkingInJava.util;
 
-public class CollectionData {
+import java.util.ArrayList;
+
+public class CollectionData<T> extends ArrayList<T> {
+    public CollectionData(Generator<T> gen, int quantity) {
+        for(int i = 0; i < quantity; i++) {
+            add(gen.next());
+        }
+    }
+
+    public static <T> CollectionData<T> list(Generator<T> gen, int quantity) {
+        return new CollectionData<T>(gen, quantity);
+    }
 }
--- thinkingInJava/util/Generated.java
+++ thinkingInJava/util/Generated.java
@@ -1,8 +1,15 @@
 package thinkingInJava.util;
 
+import java.lang.reflect.Array;
+
 public class Generated {
     //Fill an existing array
     public static <T> T[] array(T[] a, Generator<T> gen) {
-        return new CollectionData<T>(gen, a.length).toArray();
+        return new CollectionData<T>(gen, a.length).toArray(a);
+    }
+
+    public static <T> T[] array(Class<T> type, Generator<T> gen, int size) {
+        T[] a = (T[]) Array.newInstance(type,size);
+        return new CollectionData<T>(gen, size).toArray(a);
     }
 }
