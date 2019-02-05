147bafc1dc9eeffc51225282f937cf08d7d0143f
Binshion <binshion@hotmail.com>
2019-02-05 19:20:25 +0800

    17.2.2 Map生成器

---
+++ thinkingInJava/util/Pair.java
@@ -0,0 +1,11 @@
+package thinkingInJava.util;
+
+public class Pair<K,V> {
+    public final K key;
+    public final V value;
+
+    public Pair(K k, V v) {
+        key = k;
+        value = v;
+    }
+}
