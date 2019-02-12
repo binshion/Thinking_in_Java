528a35c1f157ba6ffd68c83a085ce0677d1dd077
binshion <binshion@hotmail.com>
2019-02-12 12:04:09 +0800

    17.7 队列

--- Test.java
+++ Test.java
@@ -11,7 +11,7 @@ public class Test {
     public static void main(String[] args){
         BigDecimal d1 = new BigDecimal("0.8000");
         BigDecimal d2 = new BigDecimal("1");
-        BigDecimal d3 = d2.divide(d1, 4, RoundingMode.HALF_UP);
+        BigDecimal d3 = d2.divide(d1, d1.scale(), RoundingMode.HALF_UP);
         System.out.println(d3);
     }
 }
--- thinkingInJava/containers/QueueBehavior.java
+++ thinkingInJava/containers/QueueBehavior.java
@@ -1,4 +1,48 @@
 package thinkingInJava.containers;
 
+import thinkingInJava.util.Generator;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+
 public class QueueBehavior {
+    private static int count = 10;
+
+    static <T> void test(Queue<T> queue, Generator<T> gen) {
+        for (int i = 0; i < count; i++) {
+            queue.offer(gen.next());
+        }
+
+        while (queue.peek() != null) {
+            System.out.print(queue.remove() + " ");
+        }
+
+        System.out.println();
+    }
+
+    static class Gen implements Generator<String> {
+        String[] s = ("one two three four five six seven " +
+                "eight nine ten").split(" ");
+
+        int i;
+
+        @Override
+        public String next() {
+            return s[i++];
+        }
+    }
+
+    public static void main(String[] args){
+        test(new LinkedList<String>(), new Gen());
+        test(new PriorityQueue<String>(), new Gen());
+        test(new ArrayBlockingQueue<String>(count), new Gen());  //count初始容量
+        test(new ConcurrentLinkedQueue<String>(), new Gen());
+        test(new LinkedBlockingQueue<String>(), new Gen());
+        test(new PriorityBlockingQueue<String>(count), new Gen());
+    }
 }
