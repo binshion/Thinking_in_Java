2ddb2e1d44f534d9e7c029051fbf97ac0e485ef7
Binshion <binshion@hotmail.com>
2019-02-24 22:21:06 +0800

    0224

--- thinkingInJava/containers/SpringDetector.java
+++ thinkingInJava/containers/SpringDetector.java
@@ -1,6 +1,30 @@
 package thinkingInJava.containers;
 
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Map;
+
 import static thinkingInJava.util.Print.*;
 
 public class SpringDetector {
+    public static <T extends Groundhog> void detectSpring(Class<T> type) throws Exception {
+        Constructor<T> ghog = type.getConstructor(int.class);
+        Map<Groundhog,Prediction> map = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            map.put(ghog.newInstance(i), new Prediction());
+        }
+
+        print("map = " + map);
+        Groundhog gh = ghog.newInstance(3);
+        print("Looking up prediction for " + gh);
+        if (map.containsKey(gh)) {
+            print(map.get(gh));
+        } else {
+            print("Key not found: " + gh);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        detectSpring(Groundhog.class);
+    }
 }
