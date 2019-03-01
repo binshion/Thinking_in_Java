c8c26484889fa3c237322dadca68a75386242f59
binshion <binshion@hotmail.com>
2019-03-01 08:47:59 +0800

    17.9.2 为速度而散列

--- thinkingInJava/containers/SimpleHashMap.java
+++ thinkingInJava/containers/SimpleHashMap.java
@@ -1,5 +1,7 @@
 package thinkingInJava.containers;
 
+import thinkingInJava.util.Countries;
+
 import java.util.*;
 
 public class SimpleHashMap<K,V> extends AbstractMap<K,V> {
@@ -33,8 +35,40 @@ public class SimpleHashMap<K,V> extends AbstractMap<K,V> {
 
     }
 
+    public V get(Object key) {  //接收Object参数是因为要使用其hashCode()方法
+        int index = Math.abs(key.hashCode()) % SIZE;
+        if (buckets[index] == null) {
+            return null;
+        }
+
+        for (MapEntry<K,V> iPair : buckets[index]) {
+            if (iPair.getKey().equals(key)) {
+                return iPair.getValue();
+            }
+        }
+
+        return null;
+    }
+
     @Override
     public Set<Entry<K, V>> entrySet() {
-        return null;
+        Set<Map.Entry<K,V>> set = new HashSet<>();
+        for (LinkedList<MapEntry<K,V>> bucket : buckets) {
+            if (bucket == null) {
+                continue;
+            }
+            for (MapEntry<K,V> mpair : bucket) {
+                set.add(mpair);
+            }
+        }
+        return set;
+    }
+
+    public static void main(String[] args){
+        SimpleHashMap<String,String> m = new SimpleHashMap<>();
+        m.putAll(Countries.capitals(25));
+        System.out.println(m);
+        System.out.println(m.get("CHINA"));
+        System.out.println(m.entrySet());
     }
 }
