9f200882e08c0bb70795cc87f6f618afdc9f954d
binshion <binshion@hotmail.com>
2019-03-04 19:09:51 +0800

    17.9 散列与散列码  Done

--- thinkingInJava/containers/CountedString.java
+++ thinkingInJava/containers/CountedString.java
@@ -1,7 +1,11 @@
 package thinkingInJava.containers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static thinkingInJava.util.Print.*;
 
 /**
  * Creating a good hashCode().
@@ -10,4 +14,48 @@ public class CountedString {
     private static List<String> created = new ArrayList<>();
     private String s;
     private int id = 0;
+
+    public CountedString(String str) {
+        s = str;
+        created.add(s);
+
+        for (String s2 : created) {
+            if (s2.equals(s)) {
+                id++;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "String: " + s + " id: " + id + " hashCode(): " + hashCode();
+    }
+
+    public int hashCode() {
+        int result = 17;
+        result = 37 * result + s.hashCode();
+        result = 37 * result + id;
+        return result;
+    }
+
+    public boolean equals(Object o) {
+        return o instanceof CountedString &&
+                s.equals(((CountedString) o).s) &&
+                id == ((CountedString) o).id;
+    }
+
+    public static void main(String[] args){
+        Map<CountedString,Integer> map = new HashMap<>();
+        CountedString[] cs = new CountedString[5];
+        for (int i = 0; i < cs.length; i++) {
+            cs[i] = new CountedString("hi");
+            map.put(cs[i], i);
+        }
+
+        print(map);
+        for (CountedString csString : cs) {
+            print("Looking up " + csString);
+            print(map.get(csString));
+        }
+    }
 }
