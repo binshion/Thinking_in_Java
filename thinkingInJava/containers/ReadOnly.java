4b44f99d8a9e667f9eb08697db2c4b2ada918a53
binshion <binshion@hotmail.com>
2019-03-08 08:56:30 +0800

    设定Collection或Map为不可修改

--- thinkingInJava/containers/ReadOnly.java
+++ thinkingInJava/containers/ReadOnly.java
@@ -2,13 +2,35 @@ package thinkingInJava.containers;
 
 import thinkingInJava.util.Countries;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
+
+import static thinkingInJava.util.Print.*;
 
 public class ReadOnly {
-    static Collection<String> data = new ArrayList<String>(Countries.names(6));
+    static Collection<String> data = new ArrayList<>(Countries.names(6));
 
     public static void main(String[] args){
+        Collection<String> c = Collections.unmodifiableCollection(new ArrayList<>(data));
+        print(c);
 
+        //Can't change it!
+//        c.add("one");
+
+        List<String> a = Collections.unmodifiableList(new ArrayList<>(data));
+        ListIterator<String> lit = a.listIterator();
+        print(lit.next());  //读取正常
+//        lit.add("one");  //不能修改
+
+        Set<String> s = Collections.unmodifiableSet(new HashSet<>(data));
+        print(s);
+//        s.add("one");  //不能修改
+
+        Set<String> ss = Collections.unmodifiableSortedSet(new TreeSet<>(data));
+
+        Map<String,String> m = Collections.unmodifiableMap(new HashMap<>(Countries.capitals(6)));
+        print(m);
+//        m.put("Ralph", "Howdy!");  //不能修改
+
+        Map<String,String> sm = Collections.unmodifiableSortedMap(new TreeMap<>(Countries.capitals(6)));
     }
 }
