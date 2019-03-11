62085ddb8770d282083fd7d91aa10419d6ab47e9
binshion <binshion@hotmail.com>
2019-03-11 20:50:33 +0800

    快速失败

---
+++ thinkingInJava/containers/FailFast.java
@@ -0,0 +1,20 @@
+package thinkingInJava.containers;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+public class FailFast {
+    public static void main(String[] args){
+        Collection<String> c = new ArrayList<>();
+        Iterator<String> it = c.iterator();
+        c.add("An object");
+
+        try {
+            String s = it.next();
+        } catch (ConcurrentModificationException e) {
+            System.out.println(e);
+        }
+    }
+}
