28b4005b448e8ec75797f24714afe56c455e4d46
Binshion <binshion@hotmail.com>
2019-03-26 22:18:01 +0800

    20.2.2 默认值限制

---
+++ thinkingInJava/annotations/SimulatingNull.java
@@ -0,0 +1,17 @@
+package thinkingInJava.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 在默认值限制
+ * 在定义注解的时候，习惯用法设置默认值的一些方式
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SimulatingNull {
+    public int id() default -1;
+    public String description() default "";
+}
