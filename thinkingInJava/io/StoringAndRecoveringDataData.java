b450f7963877fffca608f4bc1303cdcfbe535c09
Binshion <binshion@hotmail.com>
2019-04-07 09:55:22 +0800

    18.6.5 存储和恢复数据

---
+++ thinkingInJava/io/StoringAndRecoveringDataData.java
@@ -0,0 +1,21 @@
+package thinkingInJava.io;
+
+import java.io.*;
+
+public class StoringAndRecoveringDataData {
+    public static void main(String[] args) throws IOException {
+        DataOutputStream out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("e:/JavaProject/Thinking_in_Java/thinkingInJava/io/Data.txt")));
+        out.writeDouble(3.14159);
+        out.writeUTF("That was pi");
+        out.writeDouble(1.41413);
+        out.writeUTF("Square root of 2");
+        out.close();
+
+        DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream("e:/JavaProject/Thinking_in_Java/thinkingInJava/io/Data.txt")));
+
+        System.out.println(in.readDouble());
+        System.out.println(in.readUTF());  //覆盖Java-UTF
+        System.out.println(in.readDouble());
+        System.out.println(in.readUTF());
+    }
+}
