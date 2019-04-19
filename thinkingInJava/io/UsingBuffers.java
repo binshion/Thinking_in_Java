c3eb30de5a11241d625197fdcc41ed80f0ac29cf
Binshion <binshion@hotmail.com>
2019-04-19 21:28:33 +0800

    18.10.5 缓冲器的细节

---
+++ thinkingInJava/io/UsingBuffers.java
@@ -0,0 +1,34 @@
+package thinkingInJava.io;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+
+import static thinkingInJava.util.Print.*;
+
+public class UsingBuffers {
+    /**
+     * 交换相邻字符
+     * @param buffer
+     */
+    private static void symmetricScramble(CharBuffer buffer) {
+        while (buffer.hasRemaining()) {
+            buffer.mark();
+            char c1 = buffer.get();
+            char c2 = buffer.get();
+            buffer.reset();
+            buffer.put(c2).put(c1);
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] data = "UsingBuffers".toCharArray();
+        ByteBuffer bb = ByteBuffer.allocate(data.length * 2);
+        CharBuffer cb = bb.asCharBuffer();
+        cb.put(data);
+        print(cb.rewind());
+        symmetricScramble(cb);
+        print(cb.rewind());
+        symmetricScramble(cb);
+        print(cb.rewind());
+    }
+}
