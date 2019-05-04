c2225c18ed34561f6fa27b919236069949b93c2f
Binshion <binshion@hotmail.com>
2019-05-04 12:28:37 +0800

    21.3.7 线程本地存储   ThreadLocal的是使用

---
+++ thinkingInJava/concurrency/ThreadLocalVariableHolder.java
@@ -0,0 +1,57 @@
+package thinkingInJava.concurrency;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Accessor implements Runnable {
+    private final int id;
+    public Accessor(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            ThreadLocalVariableHolder.increment();
+            System.out.println(this);
+            Thread.yield();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "#" + id + ": " + ThreadLocalVariableHolder.get();
+    }
+}
+
+/**
+ * ThreadLocal只能通过get()和set()方法来访问该对象的内容
+ */
+public class ThreadLocalVariableHolder {
+    private static ThreadLocal<Integer> value = new ThreadLocal<Integer>() {
+        private Random rand = new Random(47);
+        protected synchronized Integer initialValue() {
+            return rand.nextInt(10000);
+        }
+    };
+
+    public static void increment() {
+        value.set(value.get() + 1);
+    }
+
+    public static int get() {
+        return value.get();
+    }
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            exec.execute(new Accessor(i));
+        }
+
+        TimeUnit.SECONDS.sleep(3);
+        exec.shutdownNow();  //all accessors will quit
+    }
+}
