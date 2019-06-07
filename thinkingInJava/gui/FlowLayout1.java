ece6b2375fe18a5fd1fddc985de1496939242673
Binshion <binshion@hotmail.com>
2019-06-07 23:26:22 +0800

    22.6.2 FlowLayout

---
+++ thinkingInJava/gui/FlowLayout1.java
@@ -0,0 +1,20 @@
+package thinkingInJava.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static thinkingInJava.util.SwingConsole.*;
+
+public class FlowLayout1 extends JFrame {
+    public FlowLayout1() {
+        setLayout(new FlowLayout());
+        for (int i = 0; i < 20; i++) {
+            add(new JButton("Button " + i));
+        }
+    }
+
+    public static void main(String[] args) {
+        run(new FlowLayout1(), 300, 300);
+    }
+}
+
